e9b12b15a65f2606b657a1fcec89ac708fa33292
Mohamed Mourad <mohamed.m_2000@outlook.com>
2025-01-19 18:44:39 +0200

    new portfolio

---
+++ landing.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gridLayer = document.querySelector(".grid-layer");
+
+    const totalBoxes = 20;
+    for (let i = 0; i < totalBoxes; i++) {
+        const box = document.createElement("div");
+        gridLayer.appendChild(box);
+    }
+
+    const boxes = document.querySelectorAll(".grid-layer div");
+    boxes.forEach(box => {
+        if (Math.random() < 0.15) {
+            box.classList.add("gradient-box");
+        }
+    });
+});
